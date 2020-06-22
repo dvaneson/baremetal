dc6b02bda161ca1cc47e2de26e870b1ed68b8cf1
Donovan Ellison <dvaneson@gmail.com>
2020-06-21 21:43:57 -0700

    Formatting update

--- switching/user/user.c
+++ switching/user/user.c
@@ -23,23 +23,23 @@
 
 extern void kputc(unsigned);
 
-void kputs(char* s) {
-  while (*s) {
-    kputc(*s++);
-  }
+void kputs(char *s) {
+    while (*s) {
+        kputc(*s++);
+    }
 }
 
 void cmain() {
-  int i;
-  setWindow(2, 22, 50, 30);   // user process on right hand side
-  cls();
-  puts("in user code\n");
-  for (i=0; i<4; i++) {
-    kputs("hello, kernel console\n");
-    puts("hello, user console\n");
-  }
-  puts("\n\nUser code does not return\n");
-  for (;;) { /* Don't return! */
-  }
-  puts("This message won't appear!\n");
+    int i;
+    setWindow(2, 22, 50, 30); // user process on right hand side
+    cls();
+    puts("in user code\n");
+    for (i = 0; i < 4; i++) {
+        kputs("hello, kernel console\n");
+        puts("hello, user console\n");
+    }
+    puts("\n\nUser code does not return\n");
+    for (;;) { /* Don't return! */
+    }
+    puts("This message won't appear!\n");
 }
